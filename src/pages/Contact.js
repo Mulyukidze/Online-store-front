95e513f57eb3ac324855ef54ef30748ce5ffa30d
Mulyuk <Mulyuk98@yandex.com>
2021-03-15 01:54:21 +0500

    contact nodemailer

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,31 +1,58 @@
 import React, { Component } from 'react';
 import { Form, Button, Container } from 'react-bootstrap'
-
+import axios from "axios";
+import {SHOP_ROUTE} from "../utils/consts";
 
 export default class Contact extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            text: ''
+        };
+      }
+    emailtostate(e){
+        this.state.email = e.target.value
+    }
+    texttostate(e){
+        this.state.text = e.target.value
+    }
+    clickbutton(e){
+        let text = this.state.text
+        let email = this.state.email
+        axios.post('http://localhost:5000/api/contacts', {
+        email: email,
+        text: text
+        }).then(r => console.log(r)).catch(err => err)
+        e.preventDefault()
+        alert('Письмо отправлено!')
+        document.location.href="http://localhost:3000/"
+    }
     render() {
         return (
             <Container style={{ width: '500px'}}>
                 <h1 className="text-center">Обратная связь</h1>
                 <Form>
                     <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Label>Введите Email</Form.Label>
+                        <Form.Control type="email" placeholder="Email" onInput={(e) => this.emailtostate(e)}/>
                         <Form.Text>
-                        Мы никогда никому не передадим вашу электронную почту
+                        Мы никогда никому не передадим вашу электронную почту!
                         </Form.Text>
                     </Form.Group>
 
                     <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Текст</Form.Label>
-                        <Form.Control as="textarea" rows="3" />
+                        <Form.Label>Введите текст</Form.Label>
+                        <Form.Control as="textarea" rows="3" placeholder="Текст" onInput={e => this.texttostate(e)}/>
                     </Form.Group>
 
                     <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Согласие на обработку данных мусорами"/>
+                        <Form.Check type="checkbox" label="Согласие на обработку данных"/>
                     </Form.Group>
-                    <Button variant="primary" type="submit" >Отправиться в космос</Button>
-
+                    <Button  onClick={event => this.clickbutton(event)}
+                    variant="primary" 
+                    type="submit" >Отправить
+                    </Button>
                 </Form>
             </Container>
         )
