191eec16b1659538566c26e8636717ddb359e279
Mulyuk <Mulyuk98@yandex.com>
2021-04-25 01:46:31 +0500

    fix validation form Auth

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {Container, Form} from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
@@ -16,6 +16,52 @@ const Auth = observer(() => {
     const isLogin = location.pathname === LOGIN_ROUTE
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [emailDirty, setEmailDirty] = useState(false)
+    const [passwordDirty, setPasswordDirty] = useState(false)
+    const [emailError, setEmailError] = useState('Email не может быть пустым!')
+    const [passwordError, setPasswordError] = useState('Пароль не может быть пустым!')
+    const [formValid, setFormValid] = useState(false)
+
+    useEffect(() => {
+        if (emailError || passwordError) {
+            setFormValid(false)
+        } else {
+            setFormValid(true)
+        }
+    }, [emailError, passwordError])
+
+    const emailHandler = (e) => {
+        setEmail(e.target.value)
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(!re.test(String(e.target.value).toLowerCase())) {
+            setEmailError('Email некорректный!')
+        } else {
+            setEmailError('')
+        }
+    }
+
+    const passwordHandler = (e) => {
+        setPassword(e.target.value)
+        if (e.target.value.length < 3 || e.target.value.length > 8) {
+            setPasswordError('Пароль должен содержать от 3 до 8 символов!')
+            if (!e.target.value) {
+                setPasswordError('Пароль не может быть пустым!!')
+            }
+        } else {
+            setPasswordError('')
+        }
+    }
+
+    const blurHandler = (e) => {
+        switch (e.target.name) {
+            case 'email':
+                setEmailDirty(true)
+                break
+            case 'password':
+                setPasswordDirty(true)
+                break
+        }
+    }
 
     const click = async () => {
         try {
@@ -41,18 +87,24 @@ const Auth = observer(() => {
         >
             <Card style={{width: 600}} className="p-5">
                 <h2 className="m-auto">{isLogin ? 'Авторизация' : "Регистрация"}</h2>
+                {(emailDirty && emailError) && <div style={{color:'red'}}>{emailError}</div>}
                 <Form className="d-flex flex-column">
                     <Form.Control
+                        name='email'
                         className="mt-3"
                         placeholder="Введите ваш email..."
                         value={email}
-                        onChange={e => setEmail(e.target.value)}
+                        onChange={e => emailHandler(e)}
+                        onBlur={e => blurHandler(e)}
                     />
+                    {(passwordDirty && passwordError) && <div style={{color:'red'}}>{passwordError}</div>}
                     <Form.Control
+                        name='password'
                         className="mt-3"
                         placeholder="Введите ваш пароль..."
                         value={password}
-                        onChange={e => setPassword(e.target.value)}
+                        onChange={e => passwordHandler(e)}
+                        onBlur={e => blurHandler(e)}
                         type="password"
                     />
                     <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
@@ -65,14 +117,15 @@ const Auth = observer(() => {
                                 Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
                                 {/* Checkbox podpiska */}
                                 <div className="mt-1">
-                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked></input>
-                                    <label class="form-check-label" for="flexCheckChecked">Согласие на получение рассылки</label>
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked" defaultChecked></input>
+                                    <label className="form-check-label" htmlFor="flexCheckChecked">Согласие на получение рассылки</label>
                                 </div>
                             </div>
                         }
                         <Button
                             variant={"outline-success"}
                             onClick={click}
+                            disabled={!formValid}
                         >
                             {isLogin ? 'Войти' : 'Регистрация' }
                         </Button>
